374e8637e26143599fd1f93bbb43a2ab5e2a2195
Tutu <116677145+tutumtetwa@users.noreply.github.com>
2024-07-08 11:39:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import git
 from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 @app.route("/update_server", methods=['POST'])
 def webhook():
@@ -10,3 +11,22 @@ def webhook():
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+if form.validate_on_submit(): #already in your code file
+    user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+    db.session.add(user)
+    db.session.commit()
